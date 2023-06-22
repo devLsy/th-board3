5809e15bd4f78658b54e03643b3b90a62bc9ec2e
devlsy86 <infightersy@naver.com>
2023-06-22 21:57:51 +0900

    사용자 관련 기능 추가

--- src/main/java/study/dev/thboard3/model/BoardVo.java
+++ src/main/java/study/dev/thboard3/model/BoardVo.java
@@ -10,5 +10,9 @@ public class BoardVo extends CmmVo{
     private String title;       //제목
     private String content;     //내용
     private String userId;      //작성자 아이디
+    private char useYn;         //사용여부
     private Integer replyCnt;   //댓글개수
+
+
+
 }
