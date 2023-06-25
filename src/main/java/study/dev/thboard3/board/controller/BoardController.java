ebb707fbaa1b498ff76cc4b6967bb600288ab0c9
devlsy86 <infightersy@naver.com>
2023-06-26 06:06:30 +0900

    게시글 등록/수정 머지문으로 변경하자

--- src/main/java/study/dev/thboard3/board/controller/BoardController.java
+++ src/main/java/study/dev/thboard3/board/controller/BoardController.java
@@ -3,6 +3,7 @@ package study.dev.thboard3.board.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import study.dev.thboard3.board.model.BoardVo;
@@ -27,16 +28,7 @@ public class BoardController {
         return mv;
     }
 
-    /**
-     * 게시글 상세
-     * @param boardSno
-     * @return
-     */
-//    @GetMapping("/detail/{boardSno}")
-//    public ModelAndView detail(@PathVariable Long boardSno) throws Exception {
-//        ModelAndView mv = new ModelAndView("pages/board/detail").addObject("info", boardService.getBoardDetail(boardSno));
-//        return mv;
-//    }
+
 
 
 }
--- src/main/java/study/dev/thboard3/board/controller/BoardRestController.java
+++ src/main/java/study/dev/thboard3/board/controller/BoardRestController.java
@@ -4,10 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import study.dev.thboard3.board.model.BoardVo;
 import study.dev.thboard3.board.service.BoardService;
 
@@ -31,5 +28,16 @@ public class BoardRestController {
     @PostMapping("/reg")
     public ResponseEntity reg(@RequestBody @Valid BoardVo boardVo, BindingResult br) throws Exception {
         return boardService.insertBoardProc(boardVo, br);
+    }
+
+    /**
+     * 게시글 상세
+     * @param boardSno
+     * @return
+     * @throws Exception
+     */
+    @GetMapping("/{boardSno}")
+    public ResponseEntity detail(@PathVariable Long boardSno) throws Exception {
+        return boardService.getBoardDetail(boardSno);
     }
 }
