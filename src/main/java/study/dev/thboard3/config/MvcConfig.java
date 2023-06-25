eed109abda31a2c56398ee56d340e868322d0e1c
devlsy86 <infightersy@naver.com>
2023-06-25 14:51:30 +0900

    인터셉터 잠시 해제

--- src/main/java/study/dev/thboard3/config/MvcConfig.java
+++ src/main/java/study/dev/thboard3/config/MvcConfig.java
@@ -1,8 +1,11 @@
 package study.dev.thboard3.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import study.dev.thboard3.cmm.interceptor.LoginInterceptor;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -10,5 +13,18 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("pages/main");
+    }
+
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(loginInterceptor())
+//                .order(1)
+//                .addPathPatterns("/**")
+//                .excludePathPatterns("/user/login", "/user/logout", "/css/**", "/js/**", "/assets/**");
+//    }
+
+    @Bean
+    public LoginInterceptor loginInterceptor() {
+        return new LoginInterceptor();
     }
 }
