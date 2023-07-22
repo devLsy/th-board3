fe01d67c1d4ce6400c79d5dfddfba47f0a1c10dc
devlsy86 <infightersy@naver.com>
2023-07-22 21:18:16 +0900

    vue.js ajax 통신 위해 잠시 설정 추가

--- src/main/java/study/dev/thboard3/config/MvcConfig.java
+++ src/main/java/study/dev/thboard3/config/MvcConfig.java
@@ -1,6 +1,7 @@
 package study.dev.thboard3.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +11,14 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addRedirectViewController("/", "/board/list");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080") // 클라이언트 도메인을 여기에 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
